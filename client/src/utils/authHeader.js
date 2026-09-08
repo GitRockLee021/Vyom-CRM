731df7303b96aa05e0dd401ccf99b56e6684aabc
Ramanujam <srinivasaramanujam21@gmail.com>
2026-09-08 16:31:30 +0530

    fix: read auth token from sessionStorage too so 'Remember me' unchecked still sends Authorization header

--- client/src/utils/authHeader.js
+++ client/src/utils/authHeader.js
@@ -1,6 +1,6 @@
 const TOKEN_KEY = 'vyom_token';
 
 export function authHeaders(extra = {}) {
-  const token = localStorage.getItem(TOKEN_KEY);
+  const token = localStorage.getItem(TOKEN_KEY) || sessionStorage.getItem(TOKEN_KEY);
   return token ? { ...extra, Authorization: `Bearer ${token}` } : extra;
 }
